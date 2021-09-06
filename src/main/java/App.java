d5711c5196cf912f7079ecd889f049b534b86d22
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-09-06 11:44:32 +0300

    Built the project

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,37 +1,23 @@
 import static spark.Spark.*;
 
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 public class App {
     public static void main(String[] args) {
-        get("/hello", ((request, response) ->
-                "<!DOCTYPE html>" +
-                "<html>" +
-                "<head>" +
-                "<title>Hello Friend!</title>" +
-                "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                "</head>" +
-                "<body>" +
-                "<h1>Hello From Afar</h1>" +
-                "<p>Dear Friend,</p>" +
-                "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
-                "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-                "<p>But I like programming a lot, so I've got that going for me. </p>" +
-                "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-                "<p>Cheers,</p>" +
-                "<p>Travel Enthusiast Jane</p>" +
-                "</body>" +
-                "</html>"));
+        staticFileLocation("/public");
+        get("/", (request, response) -> {
 
-        get("/images", ((request, response)->
-                "<!DOCTYPE html>" +
-                        "<html>" +
-                        "<head>" +
-                        "<title>Hello Friend!</title>" +
-                        "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                        "</head>" +
-                        "<body>" +
-                        "<h1>Favorite Traveling Photos</h1>" +
-                        "<ul>" +
-                        "<li><img src='/home/moringa/IdeaProjects/Spark2/src/main/resources/public/images/moringaschool.png' alt='A photo of a mountain range.'/></li>" + "<li><img src='/images/2.3water-nature-ex.jpg' alt='A photo of a swamp.'/></li>" + "</ul>" + "</body>" + "</html>"
-        ));
+            return new ModelAndView(new HashMap(), "hello.hbs");
+
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/favorite_photos", (request, response) -> {
+            return new ModelAndView(new HashMap(), "favorite_photos.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
